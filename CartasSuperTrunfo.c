d7b175ab7efc8ceff3d9679bf6e542d55fa3c310
MoisesRangel <moisesrangel97@gmail.com>
2024-12-22 09:17:13 +0000

    Nivel Novato Concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,10 +9,11 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
     int populacao, pontos_turisticos;
-    float area, pib;
-    char nome_cidade[20], codigo_cidade[3]; 
-    char estado;
+    float area, pib_per_capita;
+    char estado, nome_cidade[20], codigo_cidade[3]; 
+    double pib, densidade_populacional;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -24,31 +25,34 @@ int main() {
     printf("Digite o código da cidade: ");
     scanf(" %s", codigo_cidade);
 
-    printf("Digite o nome da cidade: ");
-    scanf(" %s", nome_cidade);
-
     printf("Digite o número da população: ");
     scanf(" %d", &populacao);
+
+    printf("Digite o nome da cidade: ");
+    scanf(" %s", nome_cidade);
 
     printf("Digite o valor da area: ");
     scanf(" %f", &area);
 
     printf("Digite o PIB: ");
-    scanf(" %f", &pib);
+    scanf(" %lf", &pib);
 
     printf("Digite a quantidade de pontos turisticos: ");
     scanf(" %d", &pontos_turisticos);
 
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    printf("\nEstado: %c", estado);
+    densidade_populacional = (float) pib / populacao;
+    pib_per_capita = (float) populacao / area;
+    
     printf("\nCodigo da cidade: %s", codigo_cidade);
+    printf("\nEstado: %c", estado);
+
     printf("\nNome: %s", nome_cidade);
     printf("\nPopulação: %d", populacao);
     printf("\nÁrea: %.2f km²", area);
-    printf("\nPIB: %.2f bilhões de reais", pib);
-    printf("\nPontos turisticos: %d\n", pontos_turisticos);
+    printf("\nPIB: %.2lf bilhões de reais", pib);
+    printf("\nPontos turisticos: %d", pontos_turisticos);
+    printf("\nPib per capita: %.2lf", pib_per_capita);
+    printf("\nDensidade populacional: %.2lf \n", densidade_populacional);
 
     return 0;
 }
@@ -57,7 +61,12 @@ int main() {
 /*Estado: A
 Código da Carta: A01
 Nome da Cidade: São Paulo
-População: 12325000
+População: 12,325,000
 Área: 1521.11 km²
 PIB: 699.28 bilhões de reais
-Número de Pontos Turísticos: 50*/
+Número de Pontos Turísticos: 50
+
+Novas Propriedades Calculadas:
+Densidade Populacional: População dividida pela área da cidade.
+PIB per Capita: PIB total dividido pela população.
+*/
